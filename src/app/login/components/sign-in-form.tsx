fda696ae004f750ddb2cf1159ff5e50f8fc6a107
Airton Jr <joseairtoncostajr.com>
2026-06-11 11:52:26 -0300

    Instalado Zod e criado Card Shadnc

--- src/app/login/components/sign-in-form.tsx
+++ src/app/login/components/sign-in-form.tsx
@@ -1,5 +1,30 @@
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 function SignInForm() {
-  return <div>Sign-in-form</div>;
+  return (
+    <div>
+      <Card className="p-2">
+        <CardHeader>
+          <CardTitle>Faça Login para Acessar o Sistema.</CardTitle>
+        </CardHeader>
+        <Label>Email</Label>
+        <Input type="email" placeholder="Digite seu Email" />
+        <Label>Senha</Label>
+        <Input type="password" placeholder="Digite sua Senha" />
+
+        <Button className="bg-blue-600">Login</Button>
+      </Card>
+    </div>
+  );
 }
 
 export default SignInForm;
--- src/app/login/components/sign-up-form.tsx
+++ src/app/login/components/sign-up-form.tsx
@@ -1,5 +1,32 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 function SignUpForm() {
-  return <div>Sign-up-form</div>;
+  return (
+    <div>
+      <Card className="p-2">
+        <CardHeader>
+          <CardTitle> Crie sua Conta para Acessar o Sistema.</CardTitle>
+        </CardHeader>
+        <Label>Nome</Label>
+        <Input placeholder="Digite seu Nome" />
+        <Label>Email</Label>
+        <Input type="email" placeholder="Digite seu Email" />
+        <Label>Senha</Label>
+        <Input type="password" placeholder="Digite sua Senha" />
+        <Label>Confirmar Senha</Label>
+        <Input type="password" placeholder="Confirme sua Senha" />
+        <h4>
+          Já tem uma Conta?
+          <span className="bg-red-700">Faça seu Login Aqui</span>
+        </h4>
+
+        <Button className="bg-blue-600">Criar Conta</Button>
+      </Card>
+    </div>
+  );
 }
 
 export default SignUpForm;
